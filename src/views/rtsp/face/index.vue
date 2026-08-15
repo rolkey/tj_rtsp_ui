08428b56e346805f5e67823f38d4b75e67d4cd6c
马海军 <517409926@qq.com>
2026-08-16 05:30:16 +0800

    宽度问题

--- src/views/rtsp/face/index.vue
+++ src/views/rtsp/face/index.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="app-container">
-      <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="80px">
+      <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="90px">
          <el-form-item label="监控点标识" prop="cameraIndexCode">
             <el-input
                v-model="queryParams.cameraIndexCode"
